700744b2d4283024983fa18c4e197d3a2ab788d6
Johan Berggren <jberggren@gmail.com>
2025-04-02 15:16:38 +0200

    Add back file content card

--- src/views/File.vue
+++ src/views/File.vue
@@ -117,84 +117,87 @@ limitations under the License.
             :reverse-transition="false"
           >
             <!-- Content-->
-            <v-card variant="flat" color="transparent">
-              <v-btn
-                v-if="
-                  systemConfig.active_llms.length &&
-                  isTextFormat &&
-                  file.summaries &&
-                  !file.summaries.length &&
-                  file.filesize < genAISizeLimit
-                "
-                variant="outlined"
-                class="text-none custom-border-color"
-                @click="generateFileSummary()"
-              >
-                <v-icon class="mr-2">mdi-shimmer</v-icon>
-                Generate AI summary</v-btn
-              >
 
-              <!-- File summary -->
-              <file-summary
-                v-for="summary in file.summaries"
-                :initial-summary="summary"
-              ></file-summary>
+            <v-btn
+              v-if="
+                systemConfig.active_llms.length &&
+                isTextFormat &&
+                file.summaries &&
+                !file.summaries.length &&
+                file.filesize < genAISizeLimit
+              "
+              variant="outlined"
+              class="text-none custom-border-color"
+              @click="generateFileSummary()"
+            >
+              <v-icon class="mr-2">mdi-shimmer</v-icon>
+              Generate AI summary</v-btn
+            >
 
-              <!-- File content iframe -->
+            <!-- File summary -->
+            <file-summary
+              v-for="summary in file.summaries"
+              :initial-summary="summary"
+            ></file-summary>
+
+            <div
+              v-if="file.filesize > fileSizeLimit || !isTextFormat"
+              style="font-family: monospace; font-size: 0.9em"
+              class="ml-4"
+            >
+              <strong v-if="file.filesize > fileSizeLimit">
+                The selected file exceeds the maximum size allowed for preview.
+              </strong>
+              <strong v-else>
+                This file format isn't currently supported for preview.
+              </strong>
+              <br />
+              To examine the file, please
+              <span
+                style="text-decoration: underline; cursor: pointer"
+                @click="downloadFileTab()"
+                >download</span
+              >
+              it to your local machine.
+            </div>
+
+            <!-- File content iframe -->
+            <v-card
+              v-if="isTextFormat && file.filesize < fileSizeLimit"
+              variant="outlined"
+              class="mt-4 custom-border-color"
+            >
               <v-toolbar
-                v-if="allowedPreview"
-                color="transparent"
+                :color="$vuetify.theme.name === 'dark' ? '' : 'grey-lighten-4'"
                 density="compact"
-                class="mt-4"
               >
-                <div style="font-size: 18px">Preview content</div>
-                <v-btn
-                  variant="tonal"
-                  size="small"
-                  class="text-none ml-3"
-                  :text="
-                    showFilePreview ? 'Show raw content' : 'Render content'
-                  "
-                  @click="showFilePreview = !showFilePreview"
-                >
-                </v-btn>
+                <v-toolbar-title style="font-size: 18px">
+                  File content
+                  <v-btn
+                    v-if="allowedPreview"
+                    variant="text"
+                    size="small"
+                    class="ml-3 text-none"
+                    :text="showFilePreview ? 'Raw' : 'Preview'"
+                    @click="showFilePreview = !showFilePreview"
+                  >
+                  </v-btn>
+                </v-toolbar-title>
               </v-toolbar>
+              <v-divider></v-divider>
 
-              <div
-                style="width: 100%; overflow: hidden"
-                v-if="isTextFormat && file.filesize < fileSizeLimit"
-              >
+              <div style="width: 100%; overflow: hidden">
                 <iframe
                   sandbox
                   :src="
-                    getIframeSrc({ unsafe: allowedPreview && showFilePreview })
+                    getIframeSrc({
+                      unsafe: allowedPreview && showFilePreview,
+                    })
                   "
                   frameborder="0"
                   scrolling="yes"
                   style="width: 100%; height: 65vh"
                 ></iframe>
-              </div>
-              <div v-else>
-                <div
-                  style="font-family: monospace; font-size: 0.9em"
-                  class="ml-4"
-                >
-                  <strong v-if="file.filesize > fileSizeLimit">
-                    The selected file exceeds the maximum size allowed for
-                    preview.
-                  </strong>
-                  <strong v-else>
-                    This file format isn't currently supported for preview.
-                  </strong>
-                  <br />
-                  To examine the file, please
-                  <span
-                    style="text-decoration: underline; cursor: pointer"
-                    @click="downloadFileTab()"
-                    >download</span
-                  >
-                  it to your local machine.
-                </div>
               </div>
             </v-card>
           </v-tabs-window-item>
